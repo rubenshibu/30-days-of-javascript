5b0491ddd568c1117aac77f67d9d0c2913c65c72
rubenshibu <rubenshibu@gmail.com>
2021-01-10 19:26:37 +0530

    day 1 :construction:

--- Day 1/dayOne.js
+++ Day 1/dayOne.js
@@ -13,9 +13,31 @@ let voices = [];
 
 const getVoices = () => {
     voices = synth.getVoices();
-    console.log(voices);
+    
+    voices.forEach(voice =>{
+        const option = document.createElement('option');
+
+        option.textContent = voice.name + '('+ voice.lang +')'
+
+        option.setAttribute('data-lang',voice.lang);
+        option.setAttribute('data-name',voice.name);
+        voiceSelect.appendChild(option);
+
+    })
 };
 
 getVoices();
 
+if(synth.onvoiceschanged !== undefined){
+    synth.onvoiceschanged = getVoices;
+
+}
+
+
+//SPEAKING
+
+
+const speak = () => {
+    
+}
 
