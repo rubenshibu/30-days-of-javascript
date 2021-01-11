c5ff558246c196da31f4307d685c0d41ed74d750
rubenshibu <rubenshibu@gmail.com>
2021-01-11 13:01:02 +0530

    DAY 2 :construction:

--- Day 2/dayTwo.js
+++ Day 2/dayTwo.js
@@ -17,23 +17,6 @@ const printOutput = (num) => {
 		document.getElementById("output-value").innerText=getFormattedNumber(num);
 	}    
 }
-// function getHistory(){
-// 	return document.getElementById("history-value").innerText;
-// }
-// function printHistory(num){
-// 	document.getElementById("history-value").innerText=num;
-// }
-// function getOutput(){
-// 	return document.getElementById("output-value").innerText;
-// }
-// function printOutput(num){
-// 	if(num==""){
-// 		document.getElementById("output-value").innerText=num;
-// 	}
-// 	else{
-// 		document.getElementById("output-value").innerText=getFormattedNumber(num);
-// 	}	
-// }
 const getFormattedNumber = (num) => {
 	if(num=="-"){
 		return "";
@@ -97,3 +80,5 @@ for(var i =0;i<number.length;i++){
 		}
 	});
 }
+
+
