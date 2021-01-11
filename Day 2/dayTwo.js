02afc4b8737ace9b958e19a954c4c29b98f153a4
rubenshibu <rubenshibu@gmail.com>
2021-01-11 11:18:16 +0530

    DAY 2 :construction:

---
+++ Day 2/dayTwo.js
@@ -0,0 +1,7 @@
+const WIDTH = 500,HEIGHT=500,
+canvas=document.getElementById('canvas'),
+context = canvas.getElementById('2d');
+
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+canvas.style.background="#123456";
