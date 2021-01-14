6ccd94672e1e0acd6461ce2b89ec161f8ddaa0f6
tonalmathew <tonalmathew1999@gmail.com>
2021-01-14 16:34:16 +0530

    :dizzy: Day 5 done :+1:

---
+++ src/Day 5/index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random() * 5) + 1;
+var randomNumber2 = Math.floor(Math.random() * 5) + 1;
+var imgArr = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png", ];
+
+document.querySelector(".img1").setAttribute("src", imgArr[randomNumber1]);
+document.querySelector(".img2").setAttribute("src", imgArr[randomNumber2]);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+} else {
+    document.querySelector("h1").innerHTML = "Draw";
+}
+
+
+
+function dieThrow() {
+    location.reload();
+}
