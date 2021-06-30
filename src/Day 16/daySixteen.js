2b8858a8d5f387d1c7497edd2a4489050cb4e514
rubenshibu <rubenshibu@gmail.com>
2021-07-01 01:52:43 +0530

    added Day 16 :construction:

--- src/Day 16/daySixteen.js
+++ src/Day 16/daySixteen.js
@@ -1,15 +1,20 @@
 //Createing an object for HTML element
 var canvas = document.getElementById('canvas');
+
 //created a 2d drawing object
 var ctx = canvas.getContext('2d');
+
 //calculate the clock radius by using the height
 var radius = canvas.height / 2;
+
 //remap the x and y axis to the center of the canvas
 ctx.translate(radius, radius);
+
 //reduce clock radius by 90%
 radius = radius * 0.90;
 
-setInterval(drawClock, 1000); //run the drawClock function every second.
+//run the drawClock function every second.
+setInterval(drawClock, 1000); 
 
 function drawClock(){
     drawFace(ctx, radius);
@@ -19,7 +24,7 @@ function drawClock(){
 
 function drawFace(ctx, radius){
     var grad;
-    //draw white circle for the face
+
     ctx.beginPath();
     ctx.arc(0,0,radius,0,2*Math.PI);
     ctx.fillStyle = "White";
