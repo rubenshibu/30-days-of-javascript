906e129209475c37674bb71620e6d433c7a66c2e
rubenshibu <rubenshibu@gmail.com>
2021-07-18 21:56:04 +0530

    :zap:

--- src/Day 1/dayOne.js
+++ src/Day 1/dayOne.js
@@ -1,76 +1,69 @@
 const synth = window.speechSynthesis;
 
-const textForm = document.querySelector('form');
-const textInput = document.querySelector('#text-input');
-const voiceSelect = document.querySelector('#voice-select');
-const rate = document.querySelector('#rate');
-const rateValue = document.querySelector('#rate-value');
-const pitch = document.querySelector('#pitch');
-const pitchValue = document.querySelector('#pitch-value');
-const body = document.querySelector('body');
+const textForm = document.querySelector("form");
+const textInput = document.querySelector("#text-input");
+const voiceSelect = document.querySelector("#voice-select");
+const rate = document.querySelector("#rate");
+const rateValue = document.querySelector("#rate-value");
+const pitch = document.querySelector("#pitch");
+const pitchValue = document.querySelector("#pitch-value");
+const body = document.querySelector("body");
 
-
-var isFirefox = typeof InstallTrigger !== 'undefined';
+var isFirefox = typeof InstallTrigger !== "undefined";
 var isChrome = !!window.chrome && !!window.chrome.webstore;
-
 
 let voices = [];
 
 const getVoices = () => {
   voices = synth.getVoices();
 
-  
-  voices.forEach(voice => {
-   
-    const option = document.createElement('option');
-    
-    option.textContent = voice.name + '(' + voice.lang + ')';
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
 
-    option.setAttribute('data-lang', voice.lang);
-    option.setAttribute('data-name', voice.name);
+    option.textContent = voice.name + "(" + voice.lang + ")";
+
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
     voiceSelect.appendChild(option);
   });
 };
 if (isFirefox) {
-    getVoices();
+  getVoices();
 }
 if (isChrome) {
-    if (synth.onvoiceschanged !== undefined) {
-        synth.onvoiceschanged = getVoices;
-    }
+  if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = getVoices;
+  }
 }
 
 // Speak
 const speak = () => {
   if (synth.speaking) {
-    console.error('Already speaking...');
+    console.error("Already speaking...");
     return;
   }
-  if (textInput.value !== '') {
-    body.style.background = 'url(img/wave.gif)';
-    body.style.backgroundRepeat = 'repeat-x';
-    body.style.backgroundSize = '100% 100%';
+  if (textInput.value !== "") {
+    body.style.background = "url(img/wave.gif)";
+    body.style.backgroundRepeat = "repeat-x";
+    body.style.backgroundSize = "100% 100%";
 
     const speakText = new SpeechSynthesisUtterance(textInput.value);
 
-
-    speakText.onend = e => {
-      console.log('Done speaking...');
-      body.style.background = '#141414';
+    speakText.onend = (e) => {
+      console.log("Done speaking...");
+      body.style.background = "#141414";
     };
 
     // Speak error
-    speakText.onerror = e => {
-      console.error('Something went wrong');
+    speakText.onerror = (e) => {
+      console.error("Something went wrong");
     };
 
     // Selected voice
-    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute(
-      'data-name'
-    );
+    const selectedVoice =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
 
-   
-    voices.forEach(voice => {
+    voices.forEach((voice) => {
       if (voice.name === selectedVoice) {
         speakText.voice = voice;
       }
@@ -84,15 +77,14 @@ const speak = () => {
   }
 };
 
-
-textForm.addEventListener('submit', e => {
+textForm.addEventListener("submit", (e) => {
   e.preventDefault();
   speak();
   textInput.blur();
 });
 
-rate.addEventListener('change', e => (rateValue.textContent = rate.value));
+rate.addEventListener("change", (e) => (rateValue.textContent = rate.value));
 
-pitch.addEventListener('change', e => (pitchValue.textContent = pitch.value));
+pitch.addEventListener("change", (e) => (pitchValue.textContent = pitch.value));
 
-voiceSelect.addEventListener('change', e => speak());
+voiceSelect.addEventListener("change", (e) => speak());
