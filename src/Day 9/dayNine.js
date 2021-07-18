caae83ed6fb44f394960a3c0b97b5ebbbd938b8c
rubenshibu <rubenshibu@gmail.com>
2021-07-18 21:53:25 +0530

    :zap:

--- src/Day 9/dayNine.js
+++ src/Day 9/dayNine.js
@@ -13,11 +13,6 @@ reset.onclick = function () {
 var prevClickMicrotime = microtime(true);
 
 function microtime(get_as_float) {
-  //  discuss at: http://phpjs.org/functions/microtime/
-  // original by: Paulo Freitas
-  //   example 1: timeStamp = microtime(true);
-  //   example 1: timeStamp > 1000000000 && timeStamp < 2000000000
-  //   returns 1: true
 
   var now = new Date().getTime() / 1000;
   var s = parseInt(now, 10);
